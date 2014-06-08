cd90831b1f2ffe0d983588a6c70d2c80e29b7741
Lachlan <ldecrespigny@gmail.com>
2014-06-08 19:52:03 -0400

    made it clearer company side

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "pages/about"
+  get "pages/admin_list"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
